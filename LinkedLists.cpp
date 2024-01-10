81c9a8ad88ca37a4e8c3c5ec6cca53df2cd448fa
cebege <cbgriffin01@gmail.com>
2024-01-10 12:35:45 +0100

    Initial commit

---
+++ LinkedLists.cpp
@@ -0,0 +1,48 @@
+//
+//class LinkedList {
+//
+//private:
+//	Node* head;
+//	Node* tail;
+//	int length;
+//
+//public:
+//
+//	LinkedList(int value) {
+//		Node* newNode = new Node(value); // creating a variable that points to the node.
+//		head = newNode;
+//		tail = newNode;
+//		length = 1;
+//	};
+//
+//	void append(int value) {};
+//
+//	void prepend(int value) {};
+//
+//	bool insert(int index, int value) {};
+//
+//};
+//
+//class Node {
+//
+//	int value;
+//	Node* next;
+//
+//public:
+//
+//	Node(int value)
+//	{
+//		this->value = value;
+//		next = nullptr;
+//	}
+//
+//
+//
+//};
+//
+//int main() {
+//
+//	LinkedList* myLinkedList = new LinkedList(4);
+//
+//
+//}
